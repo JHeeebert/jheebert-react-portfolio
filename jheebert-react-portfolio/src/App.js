5ece593d18f98442b9884c0d37c808c01c14a304
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-07 02:14:09 -0400

    added portfolio.css file and refactored App.js

--- jheebert-react-portfolio/src/App.js
+++ jheebert-react-portfolio/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/navBar/navBar";
 import CustomCarousel from "./components/carousel/carousel";
-import HomeMessage from "./components/homeMessage/homeMessage";
 import About from "./components/about/about";
 import { Parallax } from "react-parallax";
 import Fade from "react-reveal/Fade";
@@ -16,62 +15,21 @@ import Footer from "./components/footer/footer";
 // App component to render the entire application
 function App() {
   return (
-    <div className="App" style={{ position: "relative" }}>
+    <div className="App">
       <NavBar />
-      <CustomCarousel />
-      <HomeMessage />
-      <div>
-        <Parallax
-          blur={{ min: -30, max: 30 }}
-          bgImage={require("./assets/img/parallax/background.jpg")}
-          bgImageAlt=""
-          strength={-200}
-        >
-          <div>
-            <Container className="container-box rounded">
-              <Fade left duration={2000}>
-                <About />
-              </Fade>
-            </Container>
-          </div>
-        </Parallax>
-      </div>
-      <div>
-        <div>
-          <Container className="container-box rounded">
-            <Fade right duration={2000}>
-              <Skills />
-            </Fade>
-          </Container>
-        </div>
-
-        <div>
-          <Container className="container-box rounded">
-            <Slide bottom duration={1000}>
-              <hr />
-              <Projects />
+      <Parallax bgImage={require("./assets/img/background/background.jpg")} bgImageAlt="background">
+        <Container>
+          <Fade left>
+            <Slide bottom>
+              <CustomCarousel />
             </Slide>
-          </Container>
-        </div>
-
-        <div>
-          <Container className="container-box rounded">
-            <Slide left duration={2000}>
-              <hr />
-            </Slide>
-          </Container>
-        </div>
-      </div>
-
-      <div>
-        <Container className="container-box rounded">
-          <Fade bottom duration={2000}>
-            <hr />
-            <Contact />
           </Fade>
         </Container>
-      </div>
-      <hr />
+      </Parallax>
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
       <Footer />
     </div>
   );
